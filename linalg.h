623e71e3013d4120fb4168de210aab83845bb0ac
Artem Goryanov <73838119+artgor261@users.noreply.github.com>
2024-12-21 22:46:24 +1000

    Delete linalg.h

--- linalg.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int **construct_matrix(int shape[2]);
-void clear(int **matrix, int shape[2]);
-void print_matrix(int **matrix, int shape[2]);
-void multiple(int **matrix_1, int **matrix_2, int **result, int shape_1[2], int shape_2[2]);
-void init(int *shape); 
-void sum(int **matrix_1, int **matrix_2, int **result, int shape[2]);
